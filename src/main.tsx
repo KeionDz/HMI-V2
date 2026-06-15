7b2b139aaf4bded9a254d89f34dc98e392738dbe
itstroysabado <gacsabado@addu.edu.ph>
2026-06-15 18:23:58 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+// Change line 5 to this:
+import { router } from "./routes/app-routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
